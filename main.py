fab3ce4c4d037eb868cd22c7f8681c838d8794ab
jc <155749885+jcsaar@users.noreply.github.com>
2024-03-16 16:55:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ print("Hello, welcome to the chord progression generator.")
 input("Please press [enter] to continue")
 
 #generating a chord set
-chord numbers = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
-random_list = [random.choice(chord numbers) for _ in range(4)]
+chord_numbers = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII"]
+random_list = [random.choice(chord_numbers) for _ in range(4)]
 
 
 print(random_list)
